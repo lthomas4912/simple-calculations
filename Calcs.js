95d0245786c65f6a0cd15e7f1f4fe88ded19f9de
Leah Thomas <leah@codedifferently.com>
2020-07-29 16:32:44 -0400

    simple calculations

---
+++ Calcs.js
@@ -0,0 +1,19 @@
+// console.log("Code Differently");
+
+// console.log(12 + 30);
+
+var x = 45;
+var y = 39;
+
+// console.log(x+y);
+
+// console.log(x-y);
+
+// console.log(x*y);
+
+// console.log(x/y);
+
+for(var i=15; i>=5; i-=2){
+    console.log(i);
+}
+
