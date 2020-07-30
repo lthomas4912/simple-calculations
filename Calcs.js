1db0d10250a98cbc0ee1f0d8e528644a550f040f
Leah Thomas <leah@codedifferently.com>
2020-07-30 16:29:44 -0400

    if/else with loops

--- Calcs.js
+++ Calcs.js
@@ -2,8 +2,8 @@
 
 // console.log(12 + 30);
 
-var x = 45;
-var y = 39;
+// var x = 45;
+// var y = 39;
 
 // console.log(x+y);
 
@@ -13,7 +13,27 @@ var y = 39;
 
 // console.log(x/y);
 
-for(var i=15; i>=5; i-=2){
-    console.log(i);
+// for(var i=15; i>=5; i-=2){
+//     console.log(i);
+// }
+
+var num1 = 393938938348;
+
+var num2 = 230923402349823;
+
+// console.log(num1-num2);
+// console.log(num1*num2);
+// console.log(num1/num2);
+// console.log(num1+num2);
+
+// Write a program that print 1-100 Multiples of 3 print "Fizz" Multiples of 5 print "Buzz"
+
+for(var i=1; i<=20; i++){
+    if(i % 3 == 0){
+        console.log("Fizz")
+    } else{
+        console.log("cool coders")
+    }
 }
 
+//If/else statements with for loops
